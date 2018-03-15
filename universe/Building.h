8a83a49e5698d66f53b9d44f4c5f701ebf6f9bf1
geoffthemedio <geoffthemedio@gmail.com>
2018-03-15 08:38:41 +0100

    grooming

--- universe/Building.h
+++ universe/Building.h
@@ -22,41 +22,31 @@ namespace Condition {
 class FO_COMMON_API Building : public UniverseObject {
 public:
     /** \name Accessors */ //@{
-    bool HostileToEmpire(int empire_id) const override;
-    std::set<std::string> Tags() const override;
+    bool                    HostileToEmpire(int empire_id) const override;
 
-    bool HasTag(const std::string& name) const override;
+    std::set<std::string>   Tags() const override;
+    bool                    HasTag(const std::string& name) const override;
+    UniverseObjectType      ObjectType() const override;
 
-    UniverseObjectType ObjectType() const override;
+    std::string             Dump(unsigned short ntabs = 0) const override;
 
-    std::string Dump(unsigned short ntabs = 0) const override;
+    int                     ContainerObjectID() const override          { return m_planet_id; }
+    bool                    ContainedBy(int object_id) const override;
 
-    int ContainerObjectID() const override
-    { return m_planet_id; }
-
-    bool ContainedBy(int object_id) const override;
+    const std::string&      BuildingTypeName() const    { return m_building_type; };        ///< returns the name of the BuildingType object for this building
+    int                     PlanetID() const            { return m_planet_id; }             ///< returns the ID number of the planet this building is on
+    int                     ProducedByEmpireID() const  { return m_produced_by_empire_id; } ///< returns the empire ID of the empire that produced this building
+    bool                    OrderedScrapped() const     { return m_ordered_scrapped; }
 
     std::shared_ptr<UniverseObject> Accept(const UniverseObjectVisitor& visitor) const override;
-
-    /** Returns the name of the BuildingType object for this building. */
-    const std::string& BuildingTypeName() const
-    { return m_building_type; };
-
-    int                     PlanetID() const            { return m_planet_id; }             ///< returns the ID number of the planet this building is on
-
-    int                     ProducedByEmpireID() const  { return m_produced_by_empire_id; } ///< returns the empire ID of the empire that produced this building
-
-    bool                    OrderedScrapped() const     { return m_ordered_scrapped; }
     //@}
 
     /** \name Mutators */ //@{
-    void Copy(std::shared_ptr<const UniverseObject> copied_object, int empire_id = ALL_EMPIRES) override;
-
-    void            SetPlanetID(int planet_id);         ///< sets the planet on which the building is located
-
-    void            Reset();                            ///< resets any building state, and removes owners
-    void            SetOrderedScrapped(bool b = true);  ///< flags building for scrapping
-
+    void Copy(std::shared_ptr<const UniverseObject> copied_object,
+              int empire_id = ALL_EMPIRES) override;
+    void SetPlanetID(int planet_id);         ///< sets the planet on which the building is located
+    void Reset();                            ///< resets any building state, and removes owners
+    void SetOrderedScrapped(bool b = true);  ///< flags building for scrapping
     void ResetTargetMaxUnpairedMeters() override;
     //@}
 
